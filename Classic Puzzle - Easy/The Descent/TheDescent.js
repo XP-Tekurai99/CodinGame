72021d40c223e5604690e2d2acdcecc4eeb474d0
Taeshawn Threatt <110959584+XP-Tekurai99@users.noreply.github.com>
2022-08-31 02:01:57 -0700

    Create TheDescent.js
    JavaScript solution to The Descent puzzle

---
+++ Classic Puzzle - Easy/The Descent/TheDescent.js
@@ -0,0 +1,29 @@
+/**
+ * while loop represents the game
+ * each iteration represents a turn of the game
+ * inputs are the heights of the mountains
+ * output is the index of the mountain to fire shoot
+ * inputs are automatically updated according to previous actions
+ **/
+
+// Game Loop
+while (true) 
+{
+    let mountainLargest = 0;
+    let mountainToShoot = 0;
+
+    for (let i = 0; i < 8; i++) 
+    {
+        const mountainH = parseInt(readline()); // height of a mountain
+
+        if (mountainLargest < mountainH)
+        {
+            mountainLargest = mountainH;
+            mountainToShoot = i;
+        }
+    }
+
+    // Action
+    console.log(mountainToShoot); // the index of the mountain to shoot
+
+}
