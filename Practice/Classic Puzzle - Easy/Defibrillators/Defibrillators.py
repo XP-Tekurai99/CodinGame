7ded5ab370a972dcc599be8696ca4adc0731c585
Taeshawn Threatt <110959584+XP-Tekurai99@users.noreply.github.com>
2022-09-08 00:39:00 -0700

    Create Defibrillators.py

---
+++ Practice/Classic Puzzle - Easy/Defibrillators/Defibrillators.py
@@ -0,0 +1,18 @@
+import sys
+import math
+
+def distance(longa, lata, longb, latb):
+    x = (longb - longa) * math.cos((lata + latb) / 2)
+    y = latb - lata
+    return 6371 * math.sqrt(x * x + y * y)
+
+lon, lat, n = float(input().replace(",",".")), float(input().replace(",",".")), int(input())
+min, res = None, None
+for i in range(n):
+    df = input().replace(",",".").split(";")
+    d = distance(lon, lat, float(df[4]), float(df[5]))
+    if (min == None) or (d < min):
+        min, res = d, df[1]
+
+#Answer
+print(res)
